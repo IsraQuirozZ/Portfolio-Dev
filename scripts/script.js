c798d4dc311da8774a349499d2bd8b1d54e15759
Israel Quiroz <israquirozz@MacBook-Pro-de-Israel.local>
2025-12-01 16:29:19 +0100

    Add Favicon

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,28 @@ toggle.addEventListener("click", () => {
   toggle.classList.toggle("show-icon");
 });
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav__link");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => link.classList.remove("active"));
+
+        const id = entry.target.getAttribute("id");
+        const activeLink = document.querySelector(`a[href="#${id}"]`);
+        activeLink.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.6,
+  }
+);
+
+sections.forEach((section) => observer.observe(section));
+
 // HOME ANIMATION
 const words = [
   "Front-End Dev",
@@ -30,3 +52,105 @@ setInterval(() => {
     wordElement.style.animation = "slide-in 0.5s ease forwards";
   }, 500);
 }, 3000);
+
+/*----------------------*/
+// VALIDACIÓN FORM
+/*----------------------*/
+const form = document.querySelector(".contact-form");
+const inputs = document.querySelectorAll(".contact-form input");
+const textArea = document.querySelector(".contact-form textarea");
+const errorMsg = document.querySelector(".error-msg");
+
+const expressions = {
+  name: /^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\s']{3,40}$/,
+  email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+};
+
+const campos = {
+  firstName: false,
+  lastName: false,
+  email: false,
+  message: false,
+};
+
+const showError = (input) => {
+  input.classList.add("invalid");
+};
+
+const clearError = (input) => {
+  input.classList.remove("invalid");
+};
+
+const formValidation = (e) => {
+  switch (e.target.name) {
+    case "firstName":
+      inputValidation(expressions.name, e.target, "firstName");
+      break;
+    case "lastName":
+      inputValidation(expressions.name, e.target, "lastName");
+      break;
+    case "email":
+      inputValidation(expressions.email, e.target, "email");
+      break;
+    case "message":
+      textareaValidation(e.target, "message");
+      break;
+  }
+};
+
+const inputValidation = (expression, input, campo) => {
+  if (expression.test(input.value.trim())) {
+    clearError(input);
+    campos[campo] = true;
+  } else {
+    showError(input);
+    campos[campo] = false;
+  }
+};
+
+const textareaValidation = (textarea, campo) => {
+  const text = textarea.value.trim();
+  const regex = /^[\wÁÉÍÓÚÜÑáéíóúüñ.,\s]+$/;
+
+  if (!regex.test(text)) {
+    showError(textarea);
+    campos[campo] = false;
+    return;
+  }
+
+  const validWords = text
+    .split(/\s+/)
+    .filter((p) => p.replace(/[.,]/g, "").length >= 2);
+
+  if (validWords.length < 3) {
+    showError(textarea);
+    campos[campo] = false;
+    return;
+  }
+
+  clearError(textarea);
+  campos[campo] = true;
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", formValidation);
+  input.addEventListener("blur", formValidation);
+});
+
+textArea.addEventListener("keyup", formValidation);
+textArea.addEventListener("blur", formValidation);
+
+form.addEventListener("submit", (e) => {
+  if (
+    campos["firstName"] &&
+    campos["lastName"] &&
+    campos["email"] &&
+    campos["message"]
+  ) {
+    alert("Enviado!");
+    e.preventDefault();
+  } else {
+    alert("Nope");
+    e.preventDefault();
+  }
+});
